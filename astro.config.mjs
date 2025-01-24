dbd312160a996d402712715b39e71d564163bf07
FenrirWolf4566 <bdezordo@gmail.com>
2025-01-24 18:27:57 +0100

    Fix Path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	// base: '/sapientiel', 
+	base: '/sapientiel', 
 	site: 'https://fenrirwolf4566.github.io',
 	integrations: [mdx(), sitemap()],
 });
